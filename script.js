115ed068bbf7aa12f00bf13be221c785208141de
adriatikal <114657778+adriatikal@users.noreply.github.com>
2024-02-02 15:19:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('butoni').onclick = function(){
+   let selektimi = document.getElementById('elm1')
+   selektimi.innerHTML='Hello there!!!'
+   selektimi.style.backgroundColor='skyblue'
+}
